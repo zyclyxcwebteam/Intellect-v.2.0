89f83520e18b07ca5dc04846cc273761deaf4ec2
skanjarla <skanjarla@zyclyx.com>
2020-05-13 11:55:27 +0530

    Add country code for phone number

--- js/career.js
+++ js/career.js
@@ -1,28 +1,30 @@
-        $(document).ready(function () {
-            $(".se-pre-con").fadeOut("slow");
-            AOS.init({
-                once: true,
-                mirror: false
-            });
-            document.getElementById("current-year").innerHTML = new Date().getFullYear();
-            //  Read job posts from DB  
-            let allPositionsElement = document.getElementById('jobPositions');
-            let path = "https://agile-plateau-09650.herokuapp.com/jobopenings";
-            let html = '';
-            fetch(path)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    localStorage.setItem("jobOpenings", data);
-                    html += data.map(function (job) {
-                        return (`<a href="./job-preview.html?id=${job.id}" class="col-12 d-flex justify-content-between py-4 job-item">
+$(document).ready(function () {
+  $(".se-pre-con").fadeOut("slow");
+  AOS.init({
+    once: true,
+    mirror: false,
+  });
+  document.getElementById("current-year").innerHTML = new Date().getFullYear();
+  //  Read job posts from DB
+  let allPositionsElement = document.getElementById("jobPositions");
+  let path = "https://agile-plateau-09650.herokuapp.com/jobopenings";
+  let html = "";
+  fetch(path)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      localStorage.setItem("jobOpenings", data);
+      html += data
+        .map(function (job) {
+          return `<a href="./job-preview.html?id=${job.id}" class="col-12 d-flex justify-content-between py-4 my-2 job-item">
                                 <h3 class="title-2 ml-3">${job.title}</h3>                                 
                                 <p><i class="fa fa-map-marker fa-fw mr-2"></i><span>${job.location}</span></p>
-    </a>`)
-                    }).join('');
-                })
-                .then(function () {
-                    allPositionsElement.innerHTML = html;
-                })
-        });
+    </a>`;
+        })
+        .join("");
+    })
+    .then(function () {
+      allPositionsElement.innerHTML = html;
+    });
+});
--- js/jobpreview.js
+++ js/jobpreview.js
@@ -56,6 +56,7 @@ UPDATE JOB DESCRIPTION PAGE CONTENT
       description.textContent = data.description;
 
       jobtitle.textContent = data.title;
+      title = data.title;
       // responsibilities
       let resHtml = "";
       if (data.requirements) {
@@ -118,23 +119,77 @@ SUBMIT JOB APPLICATION FORM DATA
 
       // if no errors send form data to the API
       if (jobApplicationForm.checkValidity()) {
-        let formData = new FormData(jobApplicationForm);
+        //        let formData = new FormData(jobApplicationForm);
+        // let data = {
+        //   firstname: formData.get("firstname"),
+        //   lastname: formData.get("lastname"),
+        //   email: formData.get("email"),
+        //   phone: formData.get("phone"),
+        //   message: formData.get("message"),
+        //   resume: formData.get("resume"),
+        //   position: jobTitle,
+        // };
+
+        const fileField = document.querySelector('input[type="file"]');
+        const firstName = document.querySelector("#firstname").value;
+        const lastName = document.querySelector("#lastname").value;
+        const email = document.querySelector("#email").value;
+        const phone = document.querySelector("#phone").value;
+        const message = document.querySelector("#message").value;
+        //const countryCode = iti.getSelectedCountryData().dialCode;
+        const countryCode = "91";
         let data = {
-          firstname: formData.get("firstname"),
-          lastname: formData.get("lastname"),
-          email: formData.get("email"),
-          phone: formData.get("phone"),
-          message: formData.get("message"),
-          resume: formData.get("resume"),
-          position: jobTitle,
+          firstname: firstName,
+          lastname: lastName,
+          email: email,
+          phone: phone,
+          subject: message,
+          position: title,
+          website: "virtech",
+          country_code: countryCode,
+          date: new Date(),
         };
 
+        // const formData = new FormData();
+        // formData.append("firstname", firstName);
+        // formData.append("lastname", lastName);
+        // formData.append("email", email);
+        // formData.append("phone", phone);
+        // formData.append("message", message);
+        // formData.append("role", title);
+        // formData.append("resume", fileField.files[0]);
+        // formData.append("website", "virtech");
+        // formData.append("countryCode", countryCode);
+
         // console.log(data);
-        fetch("https://agile-plateau-09650.herokuapp.com/jobapplications", {
+        // fetch("https://agile-plateau-09650.herokuapp.com/jobapplications", {
+        //   method: "post",
+        //   headers: {
+        //     "Content-type": "application/json",
+        //   },
+        //   body: JSON.stringify(data),
+        // })
+        //   .then(function (response) {
+        //     document
+        //       .getElementById("submitApplicationButton")
+        //       .setAttribute("disabled", true);
+        //     document.getElementById("btnText").style.display = "none";
+        //     document.getElementById("btnSpinner").style.display = "block";
+        //     return response.json();
+        //   })
+        //   .then(function (jsondata) {
+        //     console.log(jsondata);
+        //     $("#JobApplicationModal").modal("show");
+        //   })
+        //   .catch(function (error) {
+        //     document.getElementById("errorAlert").textContent =
+        //       "Something went wrong! Please try again";
+        //   });
+
+        // new strapi
+
+        fetch("https://zyclyx-backend-api.herokuapp.com/job-applications/", {
           method: "post",
-          headers: {
-            "Content-type": "application/json",
-          },
           body: JSON.stringify(data),
         })
           .then(function (response) {
@@ -146,13 +201,30 @@ SUBMIT JOB APPLICATION FORM DATA
             return response.json();
           })
           .then(function (jsondata) {
-            console.log(jsondata);
-            $("#JobApplicationModal").modal("show");
-            // document.getAnimations.contactName.textContent = jsondata.name;
+            // upload file
+            // let fileData = new FormData();
+            // fileData.append("files", fileField.files[0]);
+            // fileData.append("refId", jsondata.id);
+            // fileData.append("field", "resume");
+            // fileData.append("ref", "job-applications");
+            // fetch("https://zyclyx-backend-api.herokuapp.com/upload/", {
+            //   method: "post",
+            //   body: fileData,
+            // })
+            //   .then(function (res) {
+            //     return res;
+            //   })
+            //   .then(function (data) {
+            //     jobApplicationForm.reset();
+            //     jobApplicationForm.classList.remove("was-validated");
+            //     document.getElementById("btnText").style.display = "block";
+            //     document.getElementById("btnSpinner").style.display = "none";
+            //   });
           })
           .catch(function (error) {
             document.getElementById("errorAlert").textContent =
               "Something went wrong! Please try again";
+            console.log(error);
           });
       }
     },
